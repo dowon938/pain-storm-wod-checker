711ad9e55f4755e2f1fc68bd039609d35d95229c
Ian <dowon938@naver.com>
2026-04-03 11:04:01 +0900

    웹에서 safe-area처리

--- app/(tabs)/location.tsx
+++ app/(tabs)/location.tsx
@@ -1,15 +1,12 @@
 import CommonWebview from '@/components/ui/CommonWebview';
 import React from 'react';
-import { Platform, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from 'react-native';
 
 export default function LocationScreen() {
-  const { top } = useSafeAreaInsets();
   return (
     <View
       style={{
         flex: 1,
-        paddingTop: top - (Platform.OS === 'ios' && top > 32 ? 8 : -10),
       }}
     >
       <CommonWebview urlPath={'/location'} />
